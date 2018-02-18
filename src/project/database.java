a47a34c5e500f442dd6c39007669723d959b95b2
sadmurshid <sadmurshid@gmail.com>
2018-02-18 19:54:04 +0600

    Database implemented successfully by Adon developer

---
+++ src/project/database.java
@@ -0,0 +1,218 @@
+package project;
+
+import java.sql.*;
+
+public class database {
+
+    private static String databaseName="shop_management_database";
+    private static String [] [] tableNameAndParameter=null;
+    private static Connection conn = null;
+    private static Statement statement;
+    private static String sql;
+    private static ResultSet resultSet, rs, rs1;
+
+    public database(String address, String user, String password)
+    {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = address;
+            conn = DriverManager.getConnection(url,user,password);
+            System.out.println("conn built");
+            databaseExist(databaseName);
+            statement= conn.createStatement();
+            sql="use "+databaseName;
+            statement.executeUpdate(sql);
+            createTableNameAndParameter();
+            for(int i=0; i<tableNameAndParameter.length; i++)
+            {
+                if(!tableExist(databaseName,tableNameAndParameter[i][0]))
+                    createTable(databaseName, tableNameAndParameter[i][0], tableNameAndParameter[i][1]);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+
+/*  Method Name: createTableNameAndParameter
+    Parameter  : null
+    Return     : void
+    Purpose    : Initiate tableNameAndParameter String array which consists [tableName][parameters]
+*/
+    private static void createTableNameAndParameter()
+    {
+        tableNameAndParameter=new String[5][2];
+        tableNameAndParameter[0][0]="user";
+        tableNameAndParameter[0][1]="user_ID int not null AUTO_INCREMENT,"+
+                                     "name varchar(40),"+
+                                     "password varchar(32),"+
+                                     "phone_number varchar(11),"+
+                                     "address varchar(100),"+
+                                     "email varchar(50),"+
+                                     "backgroundColor varchar(15),"+
+                                     "inventory_access int not null,"+
+                                     "batch_access int not null,"+
+                                     "order_access int not null,"+
+                                     "debits_access int not null,"+
+                                     "PRIMARY KEY (user_ID)";
+        tableNameAndParameter[1][0]="inventory";
+        tableNameAndParameter[1][1]="product_ID int not null,"+
+                                    "company_name varchar(20) not null,"+
+                                    "product_name varchar(20) not null,"+
+                                    "serial int not null,"+
+                                    "quantity double not null,"+
+                                    "sell_price double not null,"+
+                                    "buy_price double not null,"+
+                                    "max_discount double not null,"+
+                                    "sr_name varchar(40),"+
+                                    "sr_phone_number varchar(11),"+
+                                    "starting_inventory double not null,"+
+                                    "minimum_require double,"+
+                                    "category varchar(20),"+
+                                    "product_description varchar(50),"+
+                                    "PRIMARY KEY (product_ID)";
+        tableNameAndParameter[2][0]="batch";
+        tableNameAndParameter[2][1]="product_ID int not null,"+
+                                    "buy_price double not null,"+
+                                    "quantity double not null,"+
+                                    "buy_date date not null,"+
+                                    "sr_name varchar(40),"+
+                                    "PRIMARY KEY (product_ID, buy_date)";
+        tableNameAndParameter[3][0]="orders";
+        tableNameAndParameter[3][1]="order_ID int not null AUTO_INCREMENT,"+
+                                    "product_ID int not null,"+
+                                    "product_name varchar(20) not null,"+
+                                    "quantity double not null,"+
+                                    "sell_price double not null,"+
+                                    "per_item_discount double,"+
+                                    "total_discount double not null,"+
+                                    "user_ID int not null,"+
+                                    "customer_name varchar(40),"+
+                                    "order_date date not null,"+
+                                    "amount_received double not null,"+
+                                    "amount_returned double not null,"+
+                                    "PRIMARY KEY (order_ID)";
+        tableNameAndParameter[4][0]="debits";
+        tableNameAndParameter[4][1]="customer_ID int not null,"+
+                                    "debits_ID int not null,"+
+                                    "order_ID int not null,"+
+                                    "date_given date,"+
+                                    "date_return date,"+
+                                    "PRIMARY KEY (customer_ID,debits_ID)";
+    }
+
+/*  Method Name: databaseExist
+    Parameter  : String databaseName
+    Return     : void
+    Purpose    : Check a database using the given name. If don't exist then call createDatabase to create one.
+*/
+    private static void databaseExist(String databaseName) throws Exception
+    {
+        String getDatabaseName="";
+        ResultSet resultSet=null;
+        boolean requiredDatabase=false;
+        try {
+            resultSet = conn.getMetaData().getCatalogs();
+        }
+        catch(Exception e)
+        {System.out.println(e);}
+        try{
+            while(resultSet.next())
+            {
+                getDatabaseName=resultSet.getString(1);
+                if(getDatabaseName.equals(databaseName))
+                {
+                    requiredDatabase=true;
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+        if(requiredDatabase==false){
+            createDatabase(databaseName);
+        }
+    }
+
+/*  Method Name: createDatabase
+    Parameter  : String database
+    Purpose    : Create a database using the given name
+*/
+    private static void createDatabase(String database)
+    {
+        String sql="create database "+database+" ;";
+        System.out.println(sql);
+        Statement stmt=null;
+        try{
+            stmt=conn.createStatement();
+            stmt.executeUpdate(sql);
+            System.out.println("Database created successfully");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Database creation failed\n");
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+
+/*  Method Name: tableExist
+    Parameter  : String databaseName, String tableName
+    Purpose    : Check a table whether exist by the given name
+    Return     : Boolean true if table exist false if don't exist
+*/
+    private static boolean tableExist(String databaseName, String tableName) throws Exception
+    {
+        boolean isTableExist=false;
+        try{
+            DatabaseMetaData metaData=conn.getMetaData();
+            resultSet=metaData.getTables( databaseName, null,"%", null);
+            String str;
+            while(resultSet.next())
+            {
+                str=resultSet.getString("TABLE_NAME");
+                if(str.equals(tableName)){
+                    isTableExist=true;
+                    break;
+                }
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+//        System.out.println("Table Name : "+tableName+" Existence : "+isTableExist);
+        return isTableExist;
+    }
+
+/*  Method Name: createTable
+    Parameter  : String databaseName, String tableName, String nameParameter
+                    for nameParameter the whole query inside "(" ")" must be provided
+    Purpose    : Create a table by the given name
+    Return     : Boolean true if table creation successful false if failed
+*/
+    private static boolean createTable(String databaseName, String tableName, String nameParameter)
+    {
+        boolean creationSuccessful=false;
+        try{
+            sql="use "+databaseName;
+            statement.executeUpdate(sql);
+            sql="create table "+tableName+" ("+nameParameter+");";
+//            System.out.println(sql);
+            statement.executeUpdate(sql);
+            creationSuccessful=true;
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return creationSuccessful;
+    }
+
+
+}
--- src/project/userPageController.java
+++ src/project/userPageController.java
@@ -1,5 +1,23 @@
 package project;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 public class userPageController {
 
+    private static Stage window;
+    @FXML private static BorderPane borderPaneUserPage;
+
+    public static void genericUser(String name)
+    {
+        window=new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(name+" as CLASS TEACHER");
+
+        //BorderPane settings
+
+
+    }
 }
